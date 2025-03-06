c113f29fc8861f58f8cebcb3fd20d11e4bb2c7d3
Lunarion31 <theturtleinwater@gmail.com>
2025-03-05 21:16:34 -0500

    buttons!!!!

--- static/js/apps.js
+++ static/js/apps.js
@@ -192,11 +192,17 @@ function openAboutWindow() {
     `
       <div class="about-content" style="display: flex; justify-content: center; align-items: flex-start; margin-top: 4px;">
         <img src="https://whale.lat/static/img/0950ou.png" alt="About Image" draggable="false" style="max-width: 50%; height: 50%; border: 2px inset #c8c7c7;">
-        <div class="about-text" style="border: 2px inset #fff;">
+        <div class="about-text" style="border: 2px inset #fff; height: 100px;">
           <p>Hi there! I'm Lunarion. I'm a 17-year-old high-schooler who has a passion for programming. Feel free to contact me!</p>
           <button class="test-button" onclick="openContactWindow()">Contact</button>
           <button class="test-button" onclick="openProjectWindow()">Credits</button>
           <button class="test-button" onclick="openTempWindow()">Projects</button>
+          <a href="https://whale.lat" target="_blank">
+          <button class="test-button" style="width: 85px; height: 25px; padding: 0; display: flex; justify-content: center; align-items: center; background: #008080">
+            <img src="/static/img/buttons/lumi.png" style="max-width: 100%; max-height: 100%;">
+          </button>
+          </a>
+        </div>
         </div>
       </div>
     `,
@@ -497,13 +503,12 @@ function openTestWindow() {
     "Test",
     ` 
     <div class="about-content" style="display: flex; justify-content: left; align-items: flex-start; margin-top: 2px;">
-        <img src="/static/img/placeholder.png" alt="About Image" draggable="false" style="max-width: 30%; height: 30%; border: 2px outset #c8c7c7;">
+      <img src="/static/img/placeholder.png" alt="About Image" draggable="false" style="max-width: 30%; height: 30%; border: 2px outset #c8c7c7;">
         <div class="about-text" style="border: 2px inset #fff; margin-left: 5px; width: 300px;">
-          <p>Test<a href="https://google.com/">Test</a></p>
-          <dl>
-          </dl>
-        </div>
+        <dl>
+        </dl>
       </div>
+    </div>
     `,
     "test-window",
     "100px",
