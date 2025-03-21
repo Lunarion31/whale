f4f5794d1f9b57fd073d3c3f0720ace3deb35003
Lunarion <117928931+Lunarion31@users.noreply.github.com>
2025-03-21 00:23:21 -0500

    asdf

--- static/js/apps.js
+++ static/js/apps.js
@@ -195,8 +195,8 @@ function openAboutWindow() {
       <div class="about-content" style="display: flex; justify-content: center; align-items: flex-start; margin-top: 4px;">
         <img src="https://whale.lat/static/img/0950ou.png" alt="About Image" draggable="false" style="max-width: 50%; height: 50%; border: 2px inset #c8c7c7;">
         <div class="about-text" style="border: 2px inset #fff; height: 140px;">
-          <p>Hi there! I'm Lunarion. I'm a 17-year-old high-schooler who has a passion for programming. Feel free to contact me!</p>
-          <p>If you know me and I'm friends with you,feel free to add my button <3 </p>
+          <p>Hi there, I'm Lunarion. I'm a 17-year-old high-schooler who has a passion for programming. Feel free to contact me! 
+          If you know me and I'm friends with you, you're welcome to add my button <3 <p>
           <button class="test-button" onclick="openContactWindow()">Contact</button>
           <button class="test-button" onclick="openProjectWindow()">Credits</button>
           <button class="test-button" onclick="openTempWindow()">Projects</button>
